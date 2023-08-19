ff563c587afd3ed38c57e7042bcdb6b7b2953d9e
Maikl <umnov.mixail@mail.ru>
2023-08-19 15:16:46 +0400

    Сделали задачу №10 № 13 № 15

---
+++ DZ3_13/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+
+char thirdnum='0';
+System.Console.WriteLine("Введите целое число");
+string? number = Console.ReadLine();
+if ((number.Length)<3)
+    System.Console.WriteLine("Третьей цифры нет");
+else
+    {
+     thirdnum = number[2];
+     System.Console.WriteLine($"{thirdnum}");
+    }
